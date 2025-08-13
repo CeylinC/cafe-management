ff9b36fe15da7c52e8c105c48e79a3c585cd9b70
Ceylin <caltepeceylin@gmail.com>
2025-08-13 15:16:56 +0300

    feat: add category and product apis

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 var cors = require("cors");
 
 const connection = require("./services/dbConnection.services");
-const userRouter = require("./router/user.router");
+const userRoute = require("./router/user.router");
+const categoryRoute = require("./router/category.router");
+const productRoute = require("./router/product.router");
 
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/user", userRouter);
+app.use("/user", userRoute);
+app.use("/category", categoryRoute);
+app.use("/product", productRoute);
 
 module.exports = app;
